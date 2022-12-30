a12d9f439f6c3863448fad61a746976bda8d2765
Dushal Singh <120463747+creatordushal@users.noreply.github.com>
2022-12-30 17:27:10 +0530

    Adding Condition of Positive and Negative Margings

--- Profit Margin Calculator/Gross Profit.py
+++ Profit Margin Calculator/Gross Profit.py
@@ -2,4 +2,9 @@
 Revenue = input("Enter Your Revenue = ")
 COGS = input("Please,Enter the amount of Cost of Goods Sold = ")
 Gross_Profit = int(Revenue) - int(COGS)
-print("The Gross Profit will be",Gross_Profit)
+
+if Revenue > COGS :
+    print("The Gross Profit will be", Gross_Profit)
+
+else :
+    print("Negative Margin =",Gross_Profit)
