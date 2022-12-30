332b791adfd19f0cb442dd5649e023e3b1bf472a
Dushal Singh <120463747+creatordushal@users.noreply.github.com>
2022-12-30 17:32:27 +0530

    Update Gross Profit.py

--- Profit Margin Calculator/Gross Profit.py
+++ Profit Margin Calculator/Gross Profit.py
@@ -2,9 +2,4 @@
 Revenue = input("Enter Your Revenue = ")
 COGS = input("Please,Enter the amount of Cost of Goods Sold = ")
 Gross_Profit = int(Revenue) - int(COGS)
-
-if Revenue > COGS :
-    print("The Gross Profit will be", Gross_Profit)
-
-else :
-    print("Negative Margin =",Gross_Profit)
+print("The Gross Profit will be",Gross_Profit)
