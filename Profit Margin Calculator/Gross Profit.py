165b19824eb82eebfc802cc1e8da946f8c1a08c7
Dushal Singh <120463747+creatordushal@users.noreply.github.com>
2022-12-30 17:18:04 +0530

    Simple Gross Profit Calculator
    # Gross Profit Formula = Revenue – Cost of goods sold

---
+++ Profit Margin Calculator/Gross Profit.py
@@ -0,0 +1,5 @@
+# Gross Profit Formula = Revenue – Cost of goods sold
+Revenue = input("Enter Your Revenue = ")
+COGS = input("Please,Enter the amount of Cost of Goods Sold = ")
+Gross_Profit = int(Revenue) - int(COGS)
+print("The Gross Profit will be",Gross_Profit)
